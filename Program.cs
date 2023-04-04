e52801d2f4089cbdbec5b91510303a3d1fde7ae2
Mimi <mireiag77@gmail>
2023-04-04 14:38:52 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_secuencial1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random number = new Random();
+            
+
+           for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = number.Next(1, 11);
+                if(array[i] == array[i])
+                {
+                    Console.Write(array[i]);
+                    Console.Write("\t");
+
+                }
+            }
+
+        }
+    }
+}
